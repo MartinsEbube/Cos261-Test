be4591db2d31b6051a2883851bedc8af972e76bf
MartinsEbube <Martexbiz06@gmail.com>
2025-05-09 00:35:59 +0100

    Create Methodoverloadin.java
    Code example of method overloading

---
+++ Methodoverloadin.java
@@ -0,0 +1,16 @@
+// 12. Method Overloading example
+public class OverloadExample {
+    void greet() {
+        System.out.println("Hello!");
+    }
+
+    void greet(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void main(String[] args) {
+        OverloadExample obj = new OverloadExample();
+        obj.greet();
+        obj.greet("Ada");
+    }
+}
