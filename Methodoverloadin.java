9934f350771e0689133c9133bd99fcd8b1769ede
MartinsEbube <Martexbiz06@gmail.com>
2025-05-09 00:59:42 +0100

    Update Methodoverloadin.java

--- Methodoverloadin.java
+++ Methodoverloadin.java
@@ -1,16 +1,30 @@
-// 12. Method Overloading example
-public class OverloadExample {
-    void greet() {
-        System.out.println("Hello!");
+public class OverloadingExample {
+
+    // Method that adds two integers
+    public int add(int a, int b) {
+        return a + b;
     }
 
-    void greet(String name) {
-        System.out.println("Hello, " + name);
+    // Method that adds three integers
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Method that adds two double numbers
+    public double add(double a, double b) {
+        return a + b;
     }
 
     public static void main(String[] args) {
-        OverloadExample obj = new OverloadExample();
-        obj.greet();
-        obj.greet("Ada");
+        OverloadingExample obj = new OverloadingExample();
+
+        // Calling the method with two integers
+        System.out.println("Sum of 2 integers: " + obj.add(10, 20));
+
+        // Calling the method with three integers
+        System.out.println("Sum of 3 integers: " + obj.add(10, 20, 30));
+
+        // Calling the method with two double numbers
+        System.out.println("Sum of 2 doubles: " + obj.add(10.5, 20.5));
     }
 }
